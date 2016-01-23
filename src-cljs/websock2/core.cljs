d14b0ef4f3e5843fcbcbea2be14e4a2699ea7fb5
esumitra <edward.sumitra@gmail.com>
2016-01-23 09:13:46 -0500

    added client websocket methods

--- src-cljs/websock2/core.cljs
+++ src-cljs/websock2/core.cljs
@@ -1,5 +1,6 @@
 (ns websock2.core
   (:require [reagent.core :as reagent :refer [atom]]
+            [websock2.websockets :as ws]
             [reagent.session :as session]
             [secretary.core :as secretary :include-macros true]
             [goog.events :as events]
@@ -8,6 +9,37 @@
             [ajax.core :refer [GET POST]])
   (:import goog.History))
 
+;;; ------------
+;;; websockets
+;;; ------------
+(defonce messages (atom []))
+(defn message-list
+  []
+  [:ul
+   (for [[i message] (map-indexed vector @messages)]
+     ^{:key i}
+     [:li message])])
+
+(defn message-input
+  []
+  (let [value (atom nil)]
+    (fn []
+      [:input.form-control
+       {:type :text
+        :placeholder "type in a message and press enter"
+        :value @value
+        :on-change #(reset! value (-> % .-target .-value))
+        :on-key-down #(when (= (.-keyCode %) 13)
+                        (ws/send-transit-msg! {:message @value})
+                        (reset! value nil))}])))
+
+(defn update-messages!
+  [{:keys [message]}]
+  (swap! messages #(vec (take 10 (conj % message)))))
+
+;;; ------------
+;;; page header
+;;; ------------
 (defn nav-link [uri title page collapsed?]
   [:li {:class (when (= page (session/get :page)) "active")}
    [:a {:href uri
@@ -35,8 +67,11 @@
          (when-not @collapsed? {:class "in"})
          [:ul.nav.navbar-nav
           [nav-link "#/" "Home" :home collapsed?]
-          [nav-link "#/about" "About" :about collapsed?]]]]])))
-
+          [nav-link "#/about" "About" :about collapsed?]
+          [nav-link "#/websocket" "Websocket" :websocket collapsed?]]]]])))
+;;; ------------
+;;; pages
+;;; ------------
 (defn about-page []
   [:div.container
    [:div.row
@@ -59,9 +94,23 @@
        [:div {:dangerouslySetInnerHTML
               {:__html (md->html docs)}}]]])])
 
+(defn websocket-page
+  []
+  [:div.container
+   [:div.row
+    [:div.col-md-12
+     [:h2 "Welcome to chat"]]]
+   [:div.row
+    [:div.col-sm-6
+     [message-list]]]
+   [:div.row
+    [:div.col-sm-6
+     [message-input]]]])
+
 (def pages
   {:home #'home-page
-   :about #'about-page})
+   :about #'about-page
+   :websocket #'websocket-page})
 
 (defn page []
   [(pages (session/get :page))])
@@ -75,6 +124,9 @@
 
 (secretary/defroute "/about" []
   (session/put! :page :about))
+
+(secretary/defroute "/websocket" []
+  (session/put! :page :websocket))
 
 ;; -------------------------
 ;; History
@@ -99,4 +151,5 @@
 (defn init! []
   (fetch-docs!)
   (hook-browser-navigation!)
+  (ws/make-websocket! (str "ws://" (.-host js/location) "/ws") update-messages!)
   (mount-components))
